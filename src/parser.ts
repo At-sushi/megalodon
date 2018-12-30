9dbfa9181ab3f2bbcbf7e62d5b988f40392d7bb5
At-sushi <dkahw210@kyoto.zaq.ne.jp>
2018-12-30 23:38:21 +0900

    Update parser.ts

--- src/parser.ts
+++ src/parser.ts
@@ -26,14 +26,14 @@ class Parser extends EventEmitter {
 
     let chunk_split: Array<string> = chunk.split('\n\n')
     
-    this.message = chunk_split.pop()
+    this.message = chunk_split.pop() as string
     chunk_split.forEach(function(piece: string) {
-      if (!piece.length) continue // empty object
+      if (!piece.length) return // empty object
 
       const root: Array<string> = piece.split('\n')
 
       // should never happen, as long as mastodon doesn't change API messages
-      if (root.length !== 2) continue
+      if (root.length !== 2) return
 
       // remove event and data markers
       const event: string = root[0].substr(7)
@@ -59,9 +59,9 @@ class Parser extends EventEmitter {
       } catch (err) {
         this.emit('error', new Error(`Error parsing API reply: '${piece}', error message: '${err}'`))
       } finally {
-        continue
+        return
       }
-    })
+    }, this)
   }
 }
 
